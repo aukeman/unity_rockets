7344e044e67e2c81b61db3869974cf88ff09e6b6
Jacob Aukeman <jacob@jacobaukeman.com>
2015-06-20 09:48:55 -0400

    initial commit

---
+++ Assets/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	Vector3 rotationVector = new Vector3( 0.0f, 0.0f, 0.0f );
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void FixedUpdate(){
+		float axis = Input.GetAxis ("Horizontal");
+
+		rotationVector.y = axis;
+
+		transform.Rotate (rotationVector);
+	
+	}
+}
