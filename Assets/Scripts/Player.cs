5492662e8162dfc3166464a3cd86b4abc3aacd5d
Jacob Aukeman <jacob@jacobaukeman.com>
2015-06-20 13:29:01 -0400

    added rocket exhaust particle system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,19 +3,43 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	Vector3 rotationVector = new Vector3( 0.0f, 0.0f, 0.0f );
+	public float maxBankAngle;
+
+	public float rotationSpeed = 1.0f;
+
+	public ParticleSystem engineExhaust;
+
+	private Vector3 rotationVector = new Vector3( 0.0f, 0.0f, 0.0f );
+	private float maxExhaustLifetime = 0.0f;
+
+	private float rotationAngle = 0.0f;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Awake(){
+		if (engineExhaust != null) {
+			engineExhaust.enableEmission = false;
+			maxExhaustLifetime = engineExhaust.startLifetime;
+		}
 	}
 
 	void FixedUpdate(){
-		float axis = Input.GetAxis ("Horizontal");
+		float rotateAxis = Input.GetAxis ("Horizontal");
+		float thrustAxis = Mathf.Max( 0f, Input.GetAxis ("Vertical") );
 
-		rotationVector.y = axis;
 
-		transform.Rotate (rotationVector);
-	
+		rotationAngle += (rotationSpeed * rotateAxis);
+
+		rotationVector.Set (0f, rotationAngle, -maxBankAngle * rotateAxis);
+
+		transform.rotation = Quaternion.Euler(rotationVector);
+
+		if (engineExhaust != null) {
+			if (0.0f < thrustAxis) {
+				engineExhaust.enableEmission = true;
+				engineExhaust.startLifetime = maxExhaustLifetime * thrustAxis;
+			} else {
+				engineExhaust.enableEmission = false;
+			}
+		}
 	}
 }
