368fa4552d3f486d92c07964fb4650f2e2ef196a
Jacob Aukeman <jacob@jacobaukeman.com>
2015-06-21 16:14:13 -0400

    add asteroid prefab

---
+++ Assets/AsteroidRotator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidRotator : MonoBehaviour {
+
+	public float minRotationSpeed = 0.1f;
+	public float maxRotationSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+
+		GetComponent<Rigidbody> ().angularVelocity = 
+			new Vector3 (Random.Range (minRotationSpeed, maxRotationSpeed),
+			             Random.Range (minRotationSpeed, maxRotationSpeed),
+			             Random.Range (minRotationSpeed, maxRotationSpeed));;
+	
+	}
+}
